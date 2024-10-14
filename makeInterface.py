1be353bc54111bf08199d616ead2fdbec96c64fc
cd-MiracleSeeker <ro.roesner@icloud.com>
2024-10-14 15:55:26 +0200

    Improved interface. Added output saving functionality.

--- makeInterface.py
+++ makeInterface.py
@@ -43,20 +43,20 @@ class WindowContent(tk.Frame):
 		self.option_frame=tk.LabelFrame(main_frame, text='Options',border=2,relief='solid',font='Helvetica 14 bold')
 		self.option_frame.pack(side='top',ipadx=20,ipady=20,padx=20,pady=20,fill='x')
 		self.option_frame.columnconfigure(0,weight=1)
-		self.option_frame.columnconfigure(1,weight=2)
+		self.option_frame.columnconfigure(1,weight=1)
 		self.option_frame.columnconfigure(2,weight=1)
-		self.option_frame.rowconfigure(1,minsize=75)
-		self.option_frame.rowconfigure(2,minsize=50)
+		#self.option_frame.rowconfigure(1,minsize=50)
+		#self.option_frame.rowconfigure(2,minsize=50)
 		
 		self.inputselect_frame=tk.LabelFrame(self.option_frame,text="Search Library by",font="Arial 14",border=0)
-		self.input_frame=tk.LabelFrame(self.option_frame,text="",border=0)
-		self.button_frame=tk.Frame(self.option_frame)
+		self.input_frame=tk.LabelFrame(self.option_frame,text="",relief='solid',border=1)
+		self.button_frame=tk.Frame(self.input_frame,relief='solid',border=0)
 		self.extraoptions_frame=tk.LabelFrame(self.option_frame,text="Additional Options",border=0)
 		
-		self.inputselect_frame.grid(column=0,row=1,ipadx=50,ipady=50,pady=20,rowspan=2,sticky='n')
-		self.input_frame.grid(column=1,row=1,ipadx=75,pady=20,sticky='n')
-		self.button_frame.grid(column=1,row=2,ipadx=75,sticky='n')
-		self.extraoptions_frame.grid(column=2,row=1,ipadx=50,ipady=5,pady=20,rowspan=2,sticky='n')
+		self.inputselect_frame.grid(column=0,row=1,padx=50,pady=20,rowspan=2,sticky='enw')
+		self.input_frame.grid(column=1,row=1,padx=30,pady=20,sticky='enw')
+		self.button_frame.grid(column=1,row=2,padx=30,pady=20,sticky='enw')
+		self.extraoptions_frame.grid(column=2,row=1,padx=50,pady=20,rowspan=2,sticky='enw')
 		
 		# output frame (bottom)
 		self.output_frame=tk.LabelFrame(main_frame, text='Requested Information will show up below',border=0,relief='solid',font='Helvetica 14 bold')
@@ -71,7 +71,8 @@ class WindowContent(tk.Frame):
 			
 			# input field for text
 			text_input=tk.Entry(self.input_frame,width=40,border=2,relief="sunken")
-			text_input.grid(row=1,column=1,sticky='en',pady=10)
+			text_input.grid(row=1,column=1,sticky='enw',pady=20,padx=20)
+			self.input_frame.columnconfigure(1, weight=1)
 			
 			return text_input
 		
@@ -91,8 +92,8 @@ class WindowContent(tk.Frame):
 				wiki_onoff.set(0)
 				simplemap_onoff.set(0)
 				complexmap_onoff.set(0)
-				selector.set("Accession Number")
-				self.input_frame.config(text="Input Accession Number")
+				selector.set("Genome Index")
+				self.input_frame.config(text="Input Genome Index")
 				self.text_field.config(width=250,height=40)
 				#self.map_field.config(width=0,height=0,border=0,relief="flat")
 				#self.map_label.config(text="")
@@ -113,10 +114,10 @@ class WindowContent(tk.Frame):
 				#generateMap(map_state,self.selection,self.query,self.text_field,self.map_field,self.map_image,self.background_image,self.render_map,self.map_label)
 			
 			
-			
-			tk.Button(self.button_frame,text='Confirm',command=lambda: confirm()).pack(side='top',fill='x',expand=1)
-			tk.Button(self.button_frame,text='Clear',command=lambda: clearText()).pack(side='left',fill='x',expand=1)
-			tk.Button(self.button_frame,text='Reset',command=lambda: reset()).pack(side='right',fill='x',expand=1)
+			ttk.Separator(self.button_frame,orient='horizontal').pack(side='top',pady=10,fill='x',expand=1)
+			ttk.Button(self.button_frame,text='Confirm',command=lambda: confirm()).pack(side='top',padx=5,fill='x',expand=1)
+			ttk.Button(self.button_frame,text='Clear',command=lambda: clearText()).pack(side='left',padx=5,fill='x',expand=1)
+			ttk.Button(self.button_frame,text='Reset',command=lambda: reset()).pack(side='right',padx=5,fill='x',expand=1)
 			
 			# make it so the input can be cofirmed by pressing return
 			self.button_frame.bind_all("<Return>",lambda x: confirm())
@@ -132,7 +133,7 @@ class WindowContent(tk.Frame):
 			
 			# set variable for storing the radiobutton selection
 			selector=tk.StringVar()
-			selector.set("Accession Number")
+			selector.set("Genome Index")
 			
 			# set label for above the input box
 			self.input_frame.config(text=f"Input {selector.get()}",font="Arial 14")
@@ -147,30 +148,43 @@ class WindowContent(tk.Frame):
 			
 			options_list=["Accession Number", "Genome Index", "Scientific Name", "Taxon Group"]
 			options_menu=tk.OptionMenu(self.inputselect_frame, selector, *(options_list), command=clicked)
-			options_menu.pack(side='top',expand=0,fill='x',pady=10)
+			options_menu.pack(side='top',expand=0,fill='x',padx=10,pady=10)
 			
 			
-			# functions for binding keyboard shortcuts
-			def select_option_accNumber(event=None):
-				selector.set("Accession Number")
+			# function for binding keyboard shortcuts
+			def select_option(selector: tk.StringVar, selection: str, event=None):
+				selector.set(selection)
 				clicked(event)
-			def select_option_speciesID(event=None):
-				selector.set("Genome Index")
-				clicked(event)
-			def select_option_sciName(event=None):
-				selector.set("Scientific Name")
-				clicked(event)
-			def select_option_taxGroup(event=None):
-				selector.set("Taxon Group")
-				clicked(event)
-			
+
 			# bind keyboard shortcuts for switching between selections
-			self.inputselect_frame.bind_all("<Command-Key-1>", select_option_accNumber)
-			self.inputselect_frame.bind_all("<Command-Key-2>", select_option_speciesID)
-			self.inputselect_frame.bind_all("<Command-Key-3>", select_option_sciName)
-			self.inputselect_frame.bind_all("<Command-Key-4>", select_option_taxGroup)
+			self.inputselect_frame.bind_all("<Command-Key-1>", lambda event: select_option(selector, "Accession Number"))
+			self.inputselect_frame.bind_all("<Command-Key-2>", lambda event: select_option(selector, "Genome Index"))
+			self.inputselect_frame.bind_all("<Command-Key-3>", lambda event: select_option(selector, "Scientific Name"))
+			self.inputselect_frame.bind_all("<Command-Key-4>", lambda event: select_option(selector, "Taxon Group"))
 			
 			return selector
+		
+		def buttonColumn():
+			from tkinter.filedialog import asksaveasfilename
+			
+			def save_output():
+				FILE_TYPES=[("Simple Text Files","*.txt"),("Complex Text Files","*.rtf")]
+				filepath=asksaveasfilename(filetypes=FILE_TYPES)
+				
+				if not filepath:
+					return
+				
+				with open(filepath,"w") as file:
+					content=self.text_field.get(1.0,tk.END)
+					file.write(content)
+			
+			ttk.Button(self.inputselect_frame,text="Open Map Editor (WIP)")
+			ttk.Button(self.inputselect_frame,text="Open Table Editor (WIP)")
+			ttk.Button(self.inputselect_frame,text="Save Output to File",command=lambda: save_output())
+			
+			for widget in self.inputselect_frame.winfo_children():
+				if '!labelframe.!button' in str(widget):
+					widget.pack(side='top',expand=0,fill='x',padx=10,pady=5)
 		
 		#function for the checkbuttons in the Options column
 		def chooseOptions():
@@ -187,12 +201,12 @@ class WindowContent(tk.Frame):
 			
 			# checkbox for enabling simple map generation
 			simplemap_onoff=tk.IntVar()
-			enable_simplemap=tk.Checkbutton(self.extraoptions_frame,text="Generate simple map (WIP)*",variable=simplemap_onoff, onvalue=1, offvalue=0)
+			enable_simplemap=tk.Checkbutton(self.extraoptions_frame,text="Generate simple map (nonfunctional)*",variable=simplemap_onoff, onvalue=1, offvalue=0)
 			enable_simplemap.grid(row=3,column=3,padx=20,sticky="nw")
 			
 			# checkbox for enabling complex map generation
 			complexmap_onoff=tk.IntVar()
-			enable_complexmap=tk.Checkbutton(self.extraoptions_frame,text="Generate complex map (WIP)*",variable=complexmap_onoff, onvalue=1, offvalue=0)
+			enable_complexmap=tk.Checkbutton(self.extraoptions_frame,text="Generate complex map (nonfunctional)*",variable=complexmap_onoff, onvalue=1, offvalue=0)
 			enable_complexmap.grid(row=4,column=3,padx=20,sticky="nw")
 			
 			# subtitle of column
@@ -219,6 +233,7 @@ class WindowContent(tk.Frame):
 		user_input=getUserInput(selector)
 		gbif_onoff,wiki_onoff,simplemap_onoff,complexmap_onoff=chooseOptions()
 		buttonRow(gbif_onoff,wiki_onoff,simplemap_onoff,complexmap_onoff,selector,user_input)
+		buttonColumn()
 	
 	def textArea(self):
 		self.text_field=tk.Text(self.output_frame,width=250,height=40,state="disabled",border=2,relief="solid",font="Arial 13",cursor="cross")
